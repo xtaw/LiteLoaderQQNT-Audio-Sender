4fc37ce5b6572f4671f73c65f8a6d8fa0c1e9b20
ShintoKosei <67397639+ShintoKosei@users.noreply.github.com>
2024-02-13 13:19:43 +0800

    Unlock

--- src/renderer.js
+++ src/renderer.js
@@ -2,24 +2,20 @@ document.addEventListener('drop', e => {
     if (document.querySelector(".audio-msg-input") != undefined) {
         e.dataTransfer.files.forEach(async file => {
             let path = file.path;
-            if (path.endsWith(".mp3") || path.endsWith(".amr") || path.endsWith(".wav") || path.endsWith(".silk") || path.endsWith(".flac") || path.endsWith(".ogg")) {
-                if (!path.endsWith(".silk")) {
-                    if (!await audiosender.convertToSilk(file.path)) {
-                        return;
-                    }
-                    path = path + '.silk';
-                }
-                const peer = await LLAPI.getPeer();
-                await LLAPI.sendMessage(peer, [
-                    {
-                        type: 'ptt',
-                        file: path
-                    }
-                ]);
-                if (!file.path.endsWith(".silk")) {
-                    audiosender.deleteFile(path);
-                }
+            const conversionSuccess = await audiosender.convertToSilk(file.path);
+            if (!conversionSuccess) {
+                console.error("File conversion failed or file type not supported.");
+                return;
+            }
+            path += '.silk';
+            const peer = await LLAPI.getPeer();
+            await LLAPI.sendMessage(peer, [{
+                type: 'ptt',
+                file: path
+            }]);
+            if (!file.path.endsWith(".silk")) {
+                audiosender.deleteFile(path);
             }
         });
     }
-})
+});
